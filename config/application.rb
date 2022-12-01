2c6c58c7d0e6f4bb42a1f3d98b9942688838d4bc
TOMURAWATARU <tomura0814@gmail.com>
2022-12-02 03:21:50 +0900

    RSpecの準備

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,12 @@ module Cosmen
     config.generators do |g|
       g.assets false
       g.test_framework false
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
