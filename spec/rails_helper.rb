fbd91b8d9bc8cd56b25bf6b32af9bebbbecb5506
TOMURAWATARU <tomura0814@gmail.com>
2022-12-06 04:22:09 +0900

    「ログインしたままにする」チェックボックス

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/rspec'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -24,7 +20,9 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+require 'capybara/rspec'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
